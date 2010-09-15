59dcd1661e34334c6ae6cf3f2464c92840daaf98
Mikael Ohlson <mikael.ohlson@timezynk.com>
2010-09-15 18:05:52 +0200

    Intellitime login functionality
      tzintellitime.module
      - two public functions tzintellitime_login() and tzintellitime_close()
      - internal methods for fetching a page and posting forms
    
      tzintellitime.test
      - test failed login with bad username or password
      - test successful login
      - test helper methods for posting and generating data
    
      test/login.php
      - script for fast testing of login function, run with "drush scr login.php"
    
      test/xpath.php
      - test xpath expressions
    
    Change-Id: Idc2bd85a48901d41ad169ea68590311958b6f9a6

---
+++ tzintellitime/test/login.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Script meant for fast testing of intellitime login code. It should be run with "drush scr login.php".
+ * It boots drupal, inserts our intellitime module, and tries to login to intellitime using that module.
+ */
+
+require_once(dirname(__FILE__) . '/../tzintellitime.module');
+
+$intelli_url = "http://my2.intelliplan.se/IntelliplanWeb/v2005/Portal/Login.aspx?Gw27UDttLdgps9TM4HqqoQ%3d%3d";
+$handle = tzintellitime_login($intelli_url, "Johan Heander", "0733623516");
+if($handle) {
+  echo "Login successful for handle " . $handle . "!\n";
+  tzintellitime_close($handle);
+} else {
+  echo "Login failed!\n";
+}
