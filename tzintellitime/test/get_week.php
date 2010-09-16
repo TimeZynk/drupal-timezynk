1416eb002ed0112103f45c5b63e5fa547bb83072
Mikael Ohlson <mikael.ohlson@timezynk.com>
2010-09-16 19:22:44 +0200

    Parse assignments and timereports
      tzintellitime.module
    
      - Refactored code into TZIntellitimeBot class that logs in, loads
        reports and assignments and TZIntellitimeUtility that does parsing
        and such.
      - Added TZIntellitimeBot::load_assignments() that loads assignments
        for a given date
      - Added TZintellitimeParser class containing misc. parser
        methods etc.
      - Added TZIntellitimeBot::logout() to allow caller to cleanup
    
      tzintellitime.test
      - Test of assignment parsing
      - Test of report parsing
    
      test/get_week.php
      - simple test script for parsing a this week
    
      test/xpath.php
      - generic xpath tester, use as php xpath.php 'query' <filename>
    
      test/intellitime-timereport-page.html
      - Data dump from intellitime session
    
    Change-Id: I41c94efbe3160a3f3068b7ef9ac3575e9dafd7bf

--- tzintellitime/test/get_week.php
+++ tzintellitime/test/get_week.php
@@ -1,17 +1,15 @@
 <?php
 
-/**
- * Script meant for fast testing of intellitime login code. It should be run with "drush scr login.php".
- * It boots drupal, inserts our intellitime module, and tries to login to intellitime using that module.
- */
-
 require_once(dirname(__FILE__) . '/../tzintellitime.module');
 
 $intelli_url = "http://my2.intelliplan.se/IntelliplanWeb/v2005/Portal/Login.aspx?Gw27UDttLdgps9TM4HqqoQ%3d%3d";
 $bot = new TZIntellitimeBot($intelli_url);
 $ok = $bot->login("Johan Heander", "0733623516");
-if($ok) {
-  echo "Login successful!\n";
-} else {
+if(!$ok) {
   echo "Login failed!\n";
+  exit();
 }
+
+$assignments = $bot->load_assignments();
+print_r($assignments);
+echo "\n";
--- tzintellitime/test/xpath.php
+++ tzintellitime/test/xpath.php
@@ -1,43 +1,10 @@
 <?php
+$query = $argv[1];
 $doc = new DOMDocument();
-$doc->loadHTMLFile($argv[1]);
-$xpath = new DOMXPath($doc);
-$actionlist = $xpath->query('//a[@href="LogOut.aspx?MId=LogOut"]');
-foreach($actionlist as $action) {
-  print_r($action);
-  print_r($action->nodeValue);
-}
+@$doc->loadHTMLFile($argv[2]);
 
-print_r(_tzintellitime_parse_form_hidden($xpath));
+print("Searching " . $argv[2] . " for query '$query'\n");
 
-$handle = curl_init();
-$post_data = array("TextBoxUserName" => "Johan Heander", 
-	           "TextBoxPassword" => "0733623516");
-_tzintellitime_post($handle, "http://my2.intelliplan.se/IntelliplanWeb/v2005/(zercof5533snfaux0nedmt55)/Portal/Login.aspx?Gw27UDttLdgps9TM4HqqoQ%3d%3d", $post_data);
-
-function _tzintellitime_parse_form_hidden($xpath) {
-  $hiddenlist = $xpath->query('//input[@type="hidden"]');
-  foreach($hiddenlist as $hidden) {
-    $name = $hidden->attributes->getNamedItem('name');
-    $value = $hidden->attributes->getNamedItem('value');
-    if($name && $value) {
-      $result[$name->nodeValue] = $value->nodeValue;
-    }
-  }
-  return $result;
-}
-
-
-function _tzintellitime_post($handle, $url, $post_data) {
-  $curl_opts = array(
-    CURLOPT_RETURNTRANSFER => TRUE,
-    CURLOPT_FAILONERROR => TRUE,
-    CURLOPT_URL => $url,
-    CURLOPT_POSTFIELDS => http_build_query($post_data),
-//    CURLOPT_POSTFIELDS => http_build_query($post_data, '', '&'),
-    CURLOPT_FOLLOWLOCATION => FALSE,
-    CURLOPT_POST => TRUE,
-  );
-  curl_setopt_array($handle, $curl_opts);
-  return curl_exec($handle);
-}
+$doc = simplexml_import_dom($doc);
+$elements = $doc->xpath($query);
+print_r($elements);
