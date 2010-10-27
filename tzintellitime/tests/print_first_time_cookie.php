e933b6570cbb6b020da47c731fbd60f288ecbb05
Mikael Ohlson <mikael.ohlson@timezynk.com>
2010-10-27 19:36:05 +0200

    143: Fixes first time sync issue.
    Adds tests and fix to ensure that we sync at the first login. The
    cause of failure was that curl writes its cookiejar when the curl
    handle is closed and we did not close the bot and curl handle after
    first login and before first sync.
    
    TZIntellitimeBot's constructor now throws an exception when the
    cookie-file is empty or missing.
    
    Change-Id: Icd3b041bb76a4b9d7785f41e59c2bc2bf1b9279c

---
+++ tzintellitime/tests/print_first_time_cookie.php
@@ -0,0 +1,20 @@
+<?php
+
+tzintellitime_include_classes();
+
+$date = time();
+if($_SERVER['argc'] == 5) {
+  $date = strtotime($_SERVER['argv'][4]);
+}
+
+$intelli_url = "https://my2.intelliplan.se/IntelliplanWeb/v2005/Portal/Login.aspx?Gw27UDttLdgps9TM4HqqoQ%3d%3d";
+$bot = new TZIntellitimeBot($intelli_url);
+$ok = $bot->login("Johan Heander", "0733623516");
+if(!$ok) {
+  echo "Login failed!\n";
+  exit();
+}
+$session_data = $bot->get_session_data();
+print_r($session_data);
+unset($bot);
+readfile($session_data['cookiejar']);
