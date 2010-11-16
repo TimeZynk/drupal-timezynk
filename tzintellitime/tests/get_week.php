84a6383825cb8977e941c0f35bbb586eb224e591
Johan Heander <johan@timezynk.com>
2010-11-16 16:04:31 +0100

    177: Better intellitime mock to enable better tests
    Done
    - Configure user login under IntellitimeSetup
    - Configure available assignments and reports under IntellitimeSetup
    - Enable and disable the comment field
    - Delay option to facilitate manual testing
    - Disabled tzintellitime_sync.test since simpletest have problems with
      keeping the changes in the test database. Solution might be on the way
      on drupal.org.
    - Manual acceptance test on tzWiki
    
    Change-Id: I21b64aa868bd23abe75bf4c5f4011f608fa4dea3

--- tzintellitime/tests/get_week.php
+++ tzintellitime/tests/get_week.php
@@ -7,17 +7,32 @@ if($_SERVER['argc'] == 5) {
   $date = date_make_date($_SERVER['argv'][4]);
 }
 
-$intelli_url = "https://my2.intelliplan.se/IntelliplanWeb/v2005/Portal/Login.aspx?Gw27UDttLdgps9TM4HqqoQ%3d%3d";
-$bot = new TZIntellitimeBot($intelli_url);
-$ok = $bot->login("Johan Heander", "0733623516");
+
+$intelli = array(
+  'url' => "https://my2.intelliplan.se/IntelliplanWeb/v2005/Portal/Login.aspx?Gw27UDttLdgps9TM4HqqoQ%3d%3d",
+  'user' => "Johan Heander",
+  'pass' => "0733623516",
+);
+
+/*
+$intelli = array(
+  'url' => "http://localhost/IntelliplanWeb/v2005/Portal/Login.aspx?Gw27UDttLdgps9TM4HqqoQ%3d%3d",
+  'user' => "test user",
+  'pass' => "test password",
+);*/
+
+$bot = new TZIntellitimeBot($intelli['url']);
+$ok = $bot->login($intelli['user'], $intelli['pass']);
 if(!$ok) {
   echo "Login failed!\n";
   exit();
 }
 $start_time = microtime(TRUE);
-$assignments = $bot->load_week($date);
+$data = $bot->load_week($date);
 $stop_time = microtime(TRUE);
-print_r($assignments);
-echo "\n";
+print_r($data);
 
-echo ($stop_time - $start_time) . "\n";
+print "\n" . serialize($data['reports']) . "\n";
+print "\n" . serialize($data['assignments']) . "\n";
+
+print "\nDuration: " . ($stop_time - $start_time) . "\n";
