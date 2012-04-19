53d4012127a7f8e4490234f1f6aa0d6aafbbf098
Johan Heander <johan@timezynk.com>
2012-04-19 20:58:03 +0200

    Sort by _lowercase_ fullname
    Change-Id: Ia51ccaae3e9ad2361eff121f424bfaec26acef30
    Reviewed-on: http://code.timezynk.com/gerrit/1077
    Reviewed-by: Johan Heander <johan@timezynk.com>
    Tested-by: Johan Heander <johan@timezynk.com>

--- tzavailability/js/collections/users.js
+++ tzavailability/js/collections/users.js
@@ -6,7 +6,11 @@ define([
         url: location.origin + '/api/users?status[10]=10&status[20]=20',
         model: User,
         comparator: function(u) {
-            return u.get('fullname');
+            var fullname = u.get('fullname');
+            if (fullname) {
+                return fullname.toLowerCase();
+            }
+            return u.id;
         }
     });
 });
